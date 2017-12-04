65b1224646fe4760121fd5cc1cbc833e25e01b4c
alex <vacheretal@eisti.eu>
2017-12-04 10:48:53 +0100

    add something

--- app/src/main/java/com/alex/android_bluetooth_beaconlibrary/activities/MainActivity.java
+++ app/src/main/java/com/alex/android_bluetooth_beaconlibrary/activities/MainActivity.java
@@ -1,7 +1,9 @@
-package com.alex.android_bluetooth_beaconlibrary;
+package com.alex.android_bluetooth_beaconlibrary.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.alex.android_bluetooth_beaconlibrary.R;
 
 public class MainActivity extends AppCompatActivity {
 
